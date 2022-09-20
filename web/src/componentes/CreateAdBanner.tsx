df8d23171ecd30030fe5802aceba39eb47ce48b8
GuilhermeMDev <guimagioni@gmail.com>
2022-09-20 19:52:33 -0300

    integration back and front-end complete

--- web/src/componentes/CreateAdBanner.tsx
+++ web/src/componentes/CreateAdBanner.tsx
@@ -1,3 +1,4 @@
+import * as Dialog from '@radix-ui/react-dialog'
 import { MagnifyingGlassPlus } from "phosphor-react";
 
 export function CreateAdBanner() {
@@ -10,9 +11,9 @@ export function CreateAdBanner() {
                     <span className='text-zinc-400'>Publique um anúncio para encontrar novos players</span>
                 </div>
 
-                <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
+                <Dialog.Trigger className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
                     <MagnifyingGlassPlus size={24} />
-                    Publicar anúncio</button>
+                    Publicar anúncio</Dialog.Trigger>
             </div>
         </div>
     )
