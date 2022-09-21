e99f2375790741e3654ce96ef6664c5253693c2f
GuilhermeMDev <guimagioni@gmail.com>
2022-09-21 19:18:17 -0300

    complete conection backand and list games

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,13 +1,22 @@
+import { useEffect, useState } from 'react';
+
 import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
-import { GAMES } from '../../utils/games';
 
 import { styles } from './styles';
 
 export function Home() {
+  const [games, setGames] = useState <GameCardProps[]>([])
+
+  useEffect(() => {
+    fetch('http://192.168.0.85:3333/games')
+      .then(response => response.json())
+      .then(data => setGames(data))
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image
@@ -21,14 +30,14 @@ export function Home() {
       />
 
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
-        renderItem={({item}) => (
+        renderItem={({ item }) => (
           <GameCard
             data={item}
           />
         )}
-        
+
         showsHorizontalScrollIndicator={false}
         horizontal
         contentContainerStyle={styles.contentList}
