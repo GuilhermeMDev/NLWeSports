6a37b6e0ed97ad34f2a05373dcc0ea73d4e4f2a8
GuilhermeMDev <guimagioni@gmail.com>
2022-09-29 15:11:12 -0300

    upload packages

--- mobile/src/screens/Game/styles.ts
+++ mobile/src/screens/Game/styles.ts
@@ -2,6 +2,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 import { StyleSheet } from 'react-native';
+import { THEME } from '../../theme';
 
 export const styles = StyleSheet.create({
   container: {
@@ -18,11 +19,11 @@ export const styles = StyleSheet.create({
     marginTop: 28,
     justifyContent: 'space-between'
   },
-  logo:{
+  logo: {
     width: 72,
     height: 40
   },
-  right:{
+  right: {
     width: 20,
     height: 20
   },
@@ -32,5 +33,27 @@ export const styles = StyleSheet.create({
     height: 160,
     borderRadius: 8,
     marginTop: 32
+  },
+
+  containerList: {
+    width: '100%',
+  },
+
+  contentList: {
+    paddingLeft: 32,
+    paddingRight: 64,
+    alignItems: 'flex-start',
+  },
+
+  emptyListText: {
+    color: THEME.COLORS.CAPTION_300,
+    fontSize: THEME.FONT_SIZE.SM,
+    fontFamily: THEME.FONT_FAMILY.REGULAR
+  },
+
+  emptyListContent: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
